54043442555c1105d9daada8ef445e28de38bf32
devopseuser <dalvir.kushwaha@outlook.com>
2024-06-20 17:59:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,11 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "{var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
